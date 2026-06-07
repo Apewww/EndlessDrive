1f421636faaf543dd94909430dfc63115d186719
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2026-06-07 13:08:43 +0700

    feat: Merapihkan struktur folder

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ if not exist bin (
 )
 
 echo Compiling Java source files to 'bin' folder...
-javac -d bin *.java
+javac -d bin src/*.java
 
 :: If compilation is successful, run the game
 if %errorlevel% equ 0 (
